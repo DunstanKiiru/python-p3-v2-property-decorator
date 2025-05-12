10c7c1a5ca5ac722a45192d75e3a8c21bb57827d
DunstanKiiru <125635215+DunstanKiiru@users.noreply.github.com>
2025-05-12 19:43:58 +0300

    Update dog.py

--- lib/dog.py
+++ lib/dog.py
@@ -18,7 +18,7 @@ class Dog:
     @property
     def name(self):
         """The name property"""
-        return self.set_name
+        return self._name
 
     @name.setter
     def name(self, name):
@@ -26,14 +26,18 @@ class Dog:
         if isinstance(name, str) and 1 <= len(name) <= 25:
             self._name = name
         else:
-            raise ValueError("Name must be string between 1 and 25 characters." )
+            raise ValueError(
+                "Name must be string between 1 and 25 characters."
+            )
 
     @property
     def breed(self):
+        """The breed property"""
         return self._breed
 
     @breed.setter
     def breed(self, breed):
+        """Breed must be in the list of approved breeds"""
         if breed in APPROVED_BREEDS:
             self._breed = breed
         else:
